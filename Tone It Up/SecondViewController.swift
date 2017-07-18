82342f6830d28310b07ac9c691ed979e8074ba84
Eric Buess <ericbuess@erics-macbook-pro.local>
2017-07-17 23:19:13 -0500

    Adds WKWebView

--- Tone It Up/SecondViewController.swift
+++ Tone It Up/SecondViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import WebKit
 
-class SecondViewController: UIViewController {
+//import SafariServices
 
+class SecondViewController: UIViewController, WKNavigationDelegate { //SFSafariViewControllerDelegate
+    
+    var webView: WKWebView!
+    
+    //    let safariVC = SFSafariViewController(url: NSURL(string: "https://my.toneitup.com/pages/daily-workout-1")! as URL)
+//    var dismissed = false
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        safariVC.delegate = self
+        let url = URL(string: "https://my.toneitup.com/pages/daily-workout-1")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        if !dismissed {
+//            self.present(safariVC, animated: false, completion: {
+//                self.dismissed = true
+//            })
+//        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+//    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+//        controller.dismiss(animated: true, completion: nil)
+//    }
 
 }
 
