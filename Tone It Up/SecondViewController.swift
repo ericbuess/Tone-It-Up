8470965b56454364a84b74aa3d00a402fcfac7ad
Eric Buess <ericbuess@erics-macbook-pro.local>
2017-07-17 23:46:43 -0500

    Removes WKWebKit and replaces it with SFSafariViewController

--- Tone It Up/SecondViewController.swift
+++ Tone It Up/SecondViewController.swift
@@ -7,48 +7,41 @@
 //
 
 import UIKit
-import WebKit
+import SafariServices
 
-//import SafariServices
+class SecondViewController: UIViewController, SFSafariViewControllerDelegate {
 
-class SecondViewController: UIViewController, WKNavigationDelegate { //SFSafariViewControllerDelegate
-    
-    var webView: WKWebView!
-    
-    //    let safariVC = SFSafariViewController(url: NSURL(string: "https://my.toneitup.com/pages/daily-workout-1")! as URL)
-//    var dismissed = false
-    
-    override func loadView() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-    }
+    let safariVC = SFSafariViewController(url: NSURL(string: "https://my.toneitup.com/pages/daily-workout-1")! as URL)
+    var dismissed = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        safariVC.delegate = self
-        let url = URL(string: "https://my.toneitup.com/pages/daily-workout-1")!
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+        safariVC.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dismissed = false
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-//        if !dismissed {
-//            self.present(safariVC, animated: false, completion: {
-//                self.dismissed = true
-//            })
-//        }
+        if !dismissed {
+            self.present(safariVC, animated: false, completion: {
+                self.dismissed = true
+                self.tabBarController?.selectedIndex = 1
+            })
+        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-//    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-//        controller.dismiss(animated: true, completion: nil)
-//    }
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        controller.dismiss(animated: false, completion: nil)
+    }
 
 }
 
