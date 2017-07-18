9a36964243c345f980f45952ca40d41858b1e0b5
Eric Buess <ericbuess@erics-macbook-pro.local>
2017-07-17 21:14:18 -0500

    Initial Commit

---
+++ Tone It UpTests/Tone_It_UpTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tone_It_UpTests.swift
+//  Tone It UpTests
+//
+//  Created by Eric Buess on 7/17/17.
+//  Copyright © 2017 Eric Buess. All rights reserved.
+//
+
+import XCTest
+@testable import Tone_It_Up
+
+class Tone_It_UpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
