c83f49c096647c509afd1c1476ecba6f1751f3dd
Eric Buess <ericbuess@erics-macbook-pro.local>
2017-07-17 22:25:15 -0500

    Adds AVKit to auto-play video at launch for prototype testing on various devices

--- Tone It Up/FirstViewController.swift
+++ Tone It Up/FirstViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import AVKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: AVPlayerViewController, AVPlayerViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        let url = URL(string: "http://www.ebookfrenzy.com/ios_book/movie/movie.mov")
+        let url = URL(string: "https://content.jwplatform.com/manifests/dON6VlzZ.m3u8")
+        
+        if let movieURL = url {
+            self.player = AVPlayer(url: movieURL)
+        }
+        
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.player?.play()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
